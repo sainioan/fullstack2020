b62ec5ef1de65c37f450a05b5c57d17d37b73914
sainioan <sainio.anniina@gmail.com>
2020-05-21 07:59:59 +0300

    update

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -31,7 +31,25 @@ const handleFilterChange = (event) => {
 
 const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(newFilter.toLowerCase()))
 
+const countriesToShow = () => {
+
+  if(filteredCountries.length < 11){
+  return(
+ <div>
+{filteredCountries
+  .map((country) => 
   
+     <div key={country.name}>{country.name}</div>
+
+  )}
+  </div>
+  )
+} else {
+  return(
+    <div>Too many matches, specify another filter</div>
+  )
+}
+}
   return (
 <div>
   <div>
@@ -40,15 +58,7 @@ const filteredCountries = countries.filter(country => country.name.toLowerCase()
      <input value = {newFilter} onChange = {handleFilterChange} >
       </input>
   </div>
-
- <div>
- {filteredCountries
-   .map((country) => 
-   
-      <div key={country.name}>{country.name}</div>
- 
-   )}
-   </div>
+{countriesToShow()}
    </div>
  )
   }
