945f1ce8c65476a6576d8f4a8b5aedb4b65eb367
sainioan <sainio.anniina@gmail.com>
2020-05-22 08:57:13 +0300

    update

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -23,29 +23,4 @@ const PersonForm = (props) => {
     )
 }
 
-/* const PersonForm = ({ addEntry, newName, handleNameChange, newNumber, handleNumberChange }) => {
-	return (
-		<div>
-			<h3>add a new </h3>
-
-			<form onSubmit={addEntry}>
-				<div>
-					name: <input value={newName} onChange={handleNameChange} required />
-				</div>
-				<div>
-					number:{' '}
-					<input
-						value={newNumber}
-						onChange={handleNumberChange}
-						required
-					/>
-				</div>
-				<div>
-					<button type="submit">add</button>
-				</div>
-			</form>
-		</div>
-	);
-}; */
-
 export default PersonForm
