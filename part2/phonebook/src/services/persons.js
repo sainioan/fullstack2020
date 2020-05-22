dae3b5b236700a34e7c6080f5962fa5ea7fdaa75
sainioan <sainio.anniina@gmail.com>
2020-05-22 07:52:38 +0300

    message

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -17,5 +17,9 @@ const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
-
-export default { getAll, create, update }
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`) 
+//	const request = axios.delete(`${baseUrl}/${id}`);
+//	return request.then((response) => response.data);
+};
+export default { getAll, create, update, remove }
