35fe5f5fba4b010157bd7d4a78cfb5b9a3fc64b9
sainioan <sainio.anniina@gmail.com>
2020-05-30 20:51:41 +0300

    update

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const blogSchema = new mongoose.Schema({
+
+  content: {
+    title: String,
+    minlength: 5,
+    required: true
+  },
+  author: { 
+    type: String,
+    minlength: 5,
+    required: true
+  }, url: { 
+    type: String,
+    minlength: 5,
+    required: true
+  },  likes: { 
+    type:  Number,
+    required: true
+  },
+ 
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
