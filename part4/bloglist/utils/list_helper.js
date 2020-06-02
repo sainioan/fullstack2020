ab928fc13911e16ba81218627896971dbe86699e
sainioan <sainio.anniina@gmail.com>
2020-06-02 16:21:04 +0300

    4.4.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  const likes = () => blogs.map(blog => blog.likes)
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+
+
+  return blogs.length === 0
+    ? 0
+    : likes().reduce(reducer)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
