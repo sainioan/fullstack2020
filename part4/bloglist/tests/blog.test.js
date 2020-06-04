bd229feaa6fce058dccdab76d151500617be94bd
sainioan <sainio.anniina@gmail.com>
2020-06-04 10:27:07 +0300

    blogTests:async_blogObjectsMap

--- part4/bloglist/tests/blog.test.js
+++ part4/bloglist/tests/blog.test.js
@@ -11,7 +11,7 @@ test('blogss are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-beforeEach(async () => {
+/* beforeEach(async () => {
   await Blog.deleteMany({})
 
   let blogObject = new Blog(helper.initialBlogs[0])
@@ -19,8 +19,15 @@ beforeEach(async () => {
 
   blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
-})
+}) */
+beforeEach(async () => {
+  await Blog.deleteMany({})
 
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
