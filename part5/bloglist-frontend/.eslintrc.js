be605a09f960bccb457deb3e4abada2471e569dd
sainioan <sainio.anniina@gmail.com>
2020-06-15 09:59:00 +0300

    cypressConfig

--- part5/bloglist-frontend/.eslintrc.js
+++ part5/bloglist-frontend/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "env": {
         "browser": true,
         "es6": true,
-        "jest/globals": true 
+        "jest/globals": true ,
+        "cypress/globals": true
     },
     "extends": [ 
         "eslint:recommended",
@@ -17,7 +18,7 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react", "jest"
+        "react", "jest", "cypress"
     ],
     "rules": {
         "indent": [
@@ -46,5 +47,10 @@ module.exports = {
         ],
         "no-console": 0,
         "react/prop-types": 0
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
     }
   }
