05caf9e264847815e468a00e495b7d49a0cfddb2
sainioan <sainio.anniina@gmail.com>
2020-07-07 11:04:14 +0300

    8.2

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -86,16 +86,35 @@ let books = [
 ]
 
 const typeDefs = gql`
+type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+  type Author {
+    name: String!
+    id: ID! 
+  }
+  enum YesNo {
+    YES
+    NO
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: (root, args) => {
+          return books
+        }
   }
 }
 
