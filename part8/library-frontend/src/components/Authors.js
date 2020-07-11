5b49396e59edf2c4d16c1c101b09b69ff82354f1
sainioan <sainio.anniina@gmail.com>
2020-07-11 09:45:15 +0300

    update

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -8,11 +8,10 @@ import { useMutation, useQuery  } from '@apollo/client'
 const Authors = (props) => {
 
 
- //const authors = props.authors
  const [name, setName] = useState('')
  const [born, setBorn] = useState('')
 
-/*  const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
+ const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
   refetchQueries: [ {query: ALL_AUTHORS}, { query:ALL_BOOKS} ],
   onError: (error) => {
     if(error.graphQLErrors.length > 0) {
@@ -21,22 +20,15 @@ const Authors = (props) => {
       console.log(error)
     }
   }
-}) */
-const [editAuthor] = useMutation(EDIT_AUTHOR)
+}) 
+// const [editAuthor] = useMutation(EDIT_AUTHOR)
 const result = useQuery(ALL_AUTHORS)
 let authors
 if (result.loading) {
   return <div>loading...</div>
 }
 authors = result.data.allAuthors
-/* const options = []
-  authors.forEach(a => options.push({ value: a.name, label: a.name }))
- */  
-/* const options = []
-  Object.entries(authors).forEach(([key, value]) => {
-    options.push({value: value.name, label: value.name})
-  })
- */
+
   let options = []
   const authorList = authors.map((author) => author.name)
 
@@ -81,26 +73,7 @@ console.log(authors)
           )}
         </tbody>
       </table>
-      <h2>set birth year</h2>
- {/*      <form onSubmit={submit}>
-        Name:{' '}
-        <Select
-          options={options}
-          value = {name}
-          onChange={setName}
-          autoFocus
-          isSearchable
-          placeholder="Search author"
-        />
-        SetBorn:{" "}
-        <input
-        type = 'text'
-          value={born}
-          onChange={({ target }) => setBorn(target.value)}
-        />
-        <button type="submit">Update Author</button>
-      </form>
-   {/*  */}   
+      <h2>Set birth year</h2>
    <form onSubmit={submit}>
         <div>
           <Select
@@ -116,25 +89,8 @@ console.log(authors)
             onChange={({ target }) => setBorn(target.value)}
           />
         </div>
-        <button type='submit'>set</button>
+        <button type='submit'>Update author</button>
       </form> 
-   {/*    <h3>Set birthyear</h3>
-          <form onSubmit={submit}>
-            <div>
-              name 
-              <select value={name} onChange={(e) => setName(e.target.value)}>
-                <option value='none'>Select author</option>
-                {authors.map(a =>
-                  <option key={a.id} value={a.name}>{a.name}</option>
-                )}
-              </select>
-              
-            </div>
-            <div>
-              born <input type='text' value={born} onChange={(e) => setBorn(e.target.value)}/>
-            </div>
-            <button type='submit'>update author</button>
-          </form> */}
     </div>
   )
 }
