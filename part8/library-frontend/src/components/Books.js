049dd8de49146633cdc6faa0e864a19a0774e411
sainioan <sainio.anniina@gmail.com>
2020-07-13 23:36:58 +0300

    8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,11 +1,10 @@
 import React, {useState} from 'react'
-import { ME } from '../queries'
-import {  useQuery} from '@apollo/client'
+
 
 
 
 const Books = (props) => {
-  const[filter, setFilter] = useState('')
+  const[filter, setFilter] = useState('all')
   const books = props.books
  
  let genres = books.map(b => b.genres).map(item => item)
@@ -16,19 +15,45 @@ const Books = (props) => {
         unique[i] = true;
       }
     });
-    return Object.keys(unique);
+    return Object.keys(unique)
   }
   
 genres = removeDups(genres)
   if (!props.show) {
     return null
   }
-  const filteredBooks = ({filter}) =>{
-    return books.filter(b => b.genres.includes(filter))
+  const allBooks = () =>{
+    return (
+      <div>
+        <h2>Books:</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>
+              Title
+              </th>
+              <th>
+              Author
+              </th>
+              <th>
+              Published
+              </th>
+            </tr>
+            {books.map(a =>
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>)
   }
   let booksToShow
-  if (filter !== '') {
+  if (filter !== 'all') {
    booksToShow= books.filter(b => b.genres.includes(filter))
+
   } else {
     booksToShow = books
   }
@@ -37,9 +62,10 @@ genres = removeDups(genres)
       <h2>Books:</h2>
       {  genres.map(item => <button key={item} onClick={() =>  setFilter(item)}>
         {item}
-      </button>)}
 
-      <table  style={{ marginTop: '16px'}}>
+      </button>)}
+      <button type = "button" onClick={() =>  setFilter('all')}>all</button>
+      <table>
         <tbody>
           <tr>
             <th>
