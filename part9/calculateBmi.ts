61e0c181bc7473a9fb2f5bd6edb33ce93d8bc2cf
sainioan <sainio.anniina@gmail.com>
2020-07-17 14:00:02 +0300

    bmi

--- part9/calculateBmi.ts
+++ part9/calculateBmi.ts
@@ -1,9 +1,29 @@
 type Result = string;
 type bmi = number;
+
+ interface Numbers {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): Numbers => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+} 
+
 const calculateBmi = (height: number, weight: number ) : Result => {
     if(weight<= 0 || height <= 0 || weight===NaN || height === NaN)
     throw new Error('invalid arguments!');
-
+    
     const bmi =weight/((height/100)*(height/100));
     console.log('bmi:', bmi)
     let value ='';
@@ -20,8 +40,10 @@ const calculateBmi = (height: number, weight: number ) : Result => {
   }
  
 
-try {
-    console.log(calculateBmi(180, 74))
-} catch (e) {
-  console.log('Something went wrong, error message: ', e.message);
-}
+  try {
+
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(weight, height));
+  } catch (e) {
+    console.log('Error, something bad happened, message: ', e.message);
+  }
