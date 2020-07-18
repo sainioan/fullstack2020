ee6da194bfb49574a76d004a65599f7a24a9d5b1
sainioan <sainio.anniina@gmail.com>
2020-07-18 14:46:51 +0300

    update

--- part9/9.1-9.7./index.ts
+++ part9/9.1-9.7./index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 const app = express();
 
-import  exerciseCalculator from './exerciseCalculator'
+import exerciseCalculator from './exerciseCalculator'
 import calculateBmi from './calculateBmi'
 import bodyParser from "body-parser";
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get('/bmi', (_req, res) => {
@@ -19,25 +20,19 @@ interface Numbers2 {
   const  body  = _req.body as Numbers2 ;
   const  daily_exercises = body.daily_exercises;
   const  target  = body.target;
-/*    const daily_exercises = _req.body.daily_exercises
-   const target = Number(_req.body.target) */
     if (!daily_exercises || !target) {
     _res.status(400).json({ error: 'parameters missing' });
    
     }
   
     if (!(daily_exercises instanceof Array) || !(typeof target === 'number')) {
-    
-      _res.status(400).json({ error: 'malformatted parameters' });
-    
+      _res.status(400).json({ error: 'malformatted parameters' });    
     }
     
     _res.json(exerciseCalculator(daily_exercises.map(Number), Number(target)));
 
   });
-  app.get('/exercises', (_req, res) => {
-  res.send(_req.body)
-  }); 
+ 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
   }); 
