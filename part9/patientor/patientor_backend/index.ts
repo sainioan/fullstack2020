300c45694d0fe8947ecf9be860133378655bf52d
sainioan <sainio.anniina@gmail.com>
2020-07-18 19:46:28 +0300

    updatePort

--- part9/patientor/patientor_backend/index.ts
+++ part9/patientor/patientor_backend/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 const app = express();
-const cors = require('cors')
+//const cors = require('cors')
+import cors from "cors";
 app.use(cors())
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
